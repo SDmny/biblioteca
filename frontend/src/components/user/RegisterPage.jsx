c2753a5614c11b440ba4c74d3e5db443b7b906f9
sianya <sianyidgm@gmail.com>
2026-03-20 19:25:38 -0600

    rol agregado

--- frontend/src/components/user/RegisterPage.jsx
+++ frontend/src/components/user/RegisterPage.jsx
@@ -1,76 +1,26 @@
 import { useNavigate, Link } from "react-router-dom";
 
 import AddUserForm from "./AddUserForm";
+import { handleCreateUser } from "../../utils/userHandler.js";
 
 import "../../styles/style_form.css";
 
 function RegisterPage() {
-
   const nav = useNavigate();
 
-
-  const guardarUsuario = (user) => {
-
-  const users =
-    JSON.parse(
-      localStorage.getItem("users")
-    ) || [];
-
-
-  users.push(user);
-
-
-  localStorage.setItem(
-    "users",
-    JSON.stringify(users)
-  );
-
-
-  // iniciar sesión automáticamente
-
-  localStorage.setItem(
-    "user",
-    JSON.stringify(user)
-  );
-
-
-  alert("Usuario creado");
-
-  nav("/catalogo");
-
-};
-
   return (
-
     <div className="form-container">
-
       <div className="form-wrapper">
-
         <div className="form-card">
-
-          <AddUserForm
-            onSubmit={guardarUsuario}
-          />
-
+          <AddUserForm onSubmit={(u) => handleCreateUser(u, nav)} />
 
           <p style={{ marginTop: 15 }}>
-
-            Ya tienes una cuenta?{" "}
-
-            <Link to="/login">
-              Iniciar sesión
-            </Link>
-
+            Ya tienes una cuenta? <Link to="/login">Iniciar sesión</Link>
           </p>
-
         </div>
-
       </div>
-
     </div>
-
   );
-
 }
 
 export default RegisterPage;
