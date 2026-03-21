b8093dc4d5f60e829de071498684ec2b1a8a1389
sianya <sianyidgm@gmail.com>
2026-03-20 19:45:52 -0600

    rutas cambiadas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Home from "./interfaces/Home.jsx";
-import Contacto from "./interfaces/Contacto.jsx";
-import Conocenos from "./interfaces/Conocenos.jsx";
-
-import Catalogo from "./interfaces/book/Catalogo.jsx";
-import BookDetailPage from "./interfaces/book/BookDetailPage.jsx";
-
-import ProfilePage from "./interfaces/user/ProfilePage.jsx";
-import ProfileView from "./interfaces/user/ProfileView.jsx";
-import Dashboard from "./interfaces/user/Dashboard.jsx";
-
-import LoginPage from "./components/user/LoginPage.jsx";
-import RegisterPage from "./components/user/RegisterPage.jsx";
 import { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -23,16 +8,16 @@ import AppRoutes from "./routes/AppRoutes.jsx";
 function App() {
   useEffect(() => {
     const defaultAdmin = {
-      nombre: "root",
-      apellido: "root",
-      usuario: "root",
-      password: "root",
+      nombre: "admin",
+      apellido: "admin",
+      usuario: "admin",
+      password: "admin",
       rol: "admin",
       email: "admin@biblioteca.com",
     };
 
     const users = JSON.parse(localStorage.getItem("users")) || [];
-    const exists = users.find((u) => u.usuario === "root");
+    const exists = users.find((u) => u.usuario === "admin");
 
     if (!exists) {
       users.push(defaultAdmin);
@@ -43,33 +28,9 @@ function App() {
   return (
     <Router>
       <Header />
-
       <div id="contenido">
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route path="/conocenos" element={<Conocenos />} />
-
-          <Route path="/contacto" element={<Contacto />} />
-
-          <Route path="/libros" element={<Catalogo />} />
-
-          <Route path="/libro/:id" element={<BookDetailPage />} />
-
-          <Route path="/login" element={<LoginPage />} />
-
-          <Route path="/register" element={<RegisterPage />} />
-
-          <Route path="/perfil" element={<ProfileView />} />
-
-          <Route path="/profile-edit" element={<ProfilePage />} />
-
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-
         <AppRoutes />
       </div>
-
       <Footer />
     </Router>
   );
