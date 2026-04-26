899b297280a3ccd7674cf033463ccdedecca14e8
Vizaith <jorgevizaithpum@gmail.com>
2026-04-26 14:25:54 -0600

    Cierre de sesión automático

--- frontend/src/utils/supabase.js
+++ frontend/src/utils/supabase.js
@@ -3,4 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    // sessionStorage, datos de sesión se borran automáticamente cuando usuario cierra pestaña o navegador
+    storage: window.sessionStorage, 
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+});
