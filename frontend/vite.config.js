2e994e8d2f3790e6a6e682034340e144fb7747a6
sianya <sianyidgm@gmail.com>
2026-05-18 09:30:31 -0600

    no minify css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync('./key.pem'),
-      cert: fs.readFileSync('./cert.pem'),
+      key: fs.readFileSync("./key.pem"),
+      cert: fs.readFileSync("./cert.pem"),
     },
-    host: 'localhost',
-  }
-})
+    host: "localhost",
+  },
+  build: {
+    cssMinify: false, // Evita que todo el CSS colapse en la línea 1
+    minify: false, // Opcional: desactiva también la minificación de JS
+  },
+});
