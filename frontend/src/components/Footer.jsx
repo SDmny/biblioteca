5eb9232da6e27f4ac103fa6d541c442c4612fa53
Vizaith <jorgevizaithpum@gmail.com>
2026-05-18 09:15:51 -0600

    Validador automatico

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,23 +4,29 @@ function Footer() {
       <footer className="bg-main text-white text-center py-3">
         &copy; 2026 Biblioteca. Todos los derechos reservados.
         <br />
-        <a href="https://validator.w3.org/nu/#textarea" target="_blank">
-          <img
-            src="https://www.w3.org/Icons/valid-html401"
-            alt="Valid HTML 5.0"
-            className="validator-badge"
-          />
-        </a>
-        <a
-          href="https://jigsaw.w3.org/css-validator/#validate-by-input"
-          target="_blank"
-        >
-          <img
-            src="https://jigsaw.w3.org/css-validator/images/vcss"
-            alt="Valid CSS"
-            className="validator-badge"
-          />
-        </a>
+  <a 
+    href="https://validator.w3.org/nu/?doc=https://main.d1ai11lwbuummm.amplifyapp.com/" 
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img
+      src="https://www.w3.org/Icons/valid-html401"
+      alt="Valid HTML 5.0"
+      className="validator-badge"
+    />
+  </a>
+
+  <a 
+   href="https://jigsaw.w3.org/css-validator/validator?uri=https://main.d1ai11lwbuummm.amplifyapp.com/" 
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img
+      src="https://jigsaw.w3.org/css-validator/images/vcss"
+      alt="Valid CSS"
+      className="validator-badge"
+    />
+  </a>
       </footer>
     </>
   );
